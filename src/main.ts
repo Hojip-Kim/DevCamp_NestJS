4c11a9516315dca2679ed6073f475a108fe75463
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-21 03:28:44 +0900

    feat
    의도 BusinessExceptionFilter 사용을위한 middleware 선언
    
    기능
    BusinessExceptionFilter 사용 가능

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BusinessExceptionFilter } from './exception/BusinessExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new BusinessExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
