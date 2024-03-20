ea2498d6ff47546f374724b4e32dfa81c9807839
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-21 03:29:59 +0900

    feat
    의도 : Entity Timestamp 공통모듈 구현
    
    기능 : TimeStamp가 필요한 Entity에 Timestamp 기능 추가 가능 모듈

---
+++ src/common/entities/base-entity.ts
@@ -0,0 +1,18 @@
+import { CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @CreateDateColumn({
+        type: 'timestamp'
+    })
+    createdAt : Date;
+
+    @UpdateDateColumn({
+        type: 'timestamp'
+    })
+    updatedAt : Date;
+}
