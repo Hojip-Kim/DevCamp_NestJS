7b2d9a6690c8cbd0e739fc3057286d99bf51981e
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-22 04:28:37 +0900

    feat
    의도
    AccessLog를 관리하는 Repository
    
    기능
    AccessLog에 대한 메서드(createAccessLog) 구현 - parameter : user, user-agent, endpoint, ip

---
+++ src/auth/repositories/access-log.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { AccessLog } from '../entities';
+import { User } from 'src/user/entities';
+
+@Injectable()
+export class AccessLogRepository extends Repository<AccessLog> {
+  constructor(
+    @InjectRepository(AccessLog)
+    private readonly repo: Repository<AccessLog>,
+    @InjectEntityManager()
+    private readonly entityManager: EntityManager,
+  ) {
+    super(repo.target, repo.manager, repo.queryRunner);
+  }
+
+  async createAccessLog(user: User, ua: string, endpoint: string, ip: string) {
+    const accessLog = new AccessLog();
+    accessLog.user = user;
+    accessLog.ua = ua;
+    accessLog.endpoint = endpoint;
+    accessLog.ip = ip;
+    accessLog.accessedAt = new Date(Date.now());
+
+    await this.repo.save(accessLog);
+  }
+}
