26c3638589e6c953ed8b8e9b82c3bc060cfe15c0
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 01:10:26 +0900

    feat
    의도
    product의 전반적인 관리를 위함
    
    기능
    product의 현재 상태 및 재고, 가격, 이미지URL 등을 관리 가능

---
+++ src/payment/entities/product.entity.ts
@@ -0,0 +1,24 @@
+import { BaseEntity, Column, Entity } from 'typeorm';
+
+export type productType = 'available' | 'not-available';
+
+@Entity()
+export class Product extends BaseEntity {
+  @Column({ type: 'decimal' })
+  price: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+
+  @Column()
+  imgURL: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ default: 'available' })
+  status: productType;
+}
