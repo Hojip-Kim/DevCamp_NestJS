393ba992ae5c638dbb8f1aa2cfeaa76bc3ba7a1c
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 01:08:28 +0900

    feat
    의도
    사용자의 point의 적립 및 사용을 기록해두기 위한 Entity
    
    기능
    point의 사용/ 적립을 관리할 수 있음.
    user => point => pointLog의 계층을 띔.

---
+++ src/payment/entities/point-log.entity.ts
@@ -0,0 +1,31 @@
+import { BaseEntity, Column, Entity, ManyToOne, Relation } from 'typeorm';
+import { Point } from './point.entity';
+
+export type PointLogType = 'earn' | 'spend';
+
+@Entity()
+export class PointLog extends BaseEntity {
+  @ManyToOne(() => Point, (point) => point.pointLog)
+  point: Relation<Point>;
+
+  @Column({ type: 'int', default: 0 })
+  amount: number; // 개별 적립 또는 사용 금액
+
+  @Column({ type: 'text' })
+  reason: string; // 개별 적립 또는 사용 사유
+
+  @Column({ type: 'varchar', length: 50 })
+  type: PointLogType;
+
+  use(amount: number, reason: string): void {
+    this.amount = amount;
+    this.reason = reason;
+    this.type = 'spend';
+  }
+
+  add(amount: number, reason: string): void {
+    this.amount = amount;
+    this.reason = reason;
+    this.type = 'earn';
+  }
+}
