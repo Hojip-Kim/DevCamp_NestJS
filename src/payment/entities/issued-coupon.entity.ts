0ea0737b5329151f4e617249e67d59eab45d7180
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 01:01:45 +0900

    feat
    의도
    발급된 쿠폰에 대한 관리를 위한 entity
    
    기능
    발급된 쿠폰 관리를 위함으로, 각 발급된 coupon을 고유한 couponID값을 주고, validation check를 함.

---
+++ src/payment/entities/issued-coupon.entity.ts
@@ -0,0 +1,64 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  Relation,
+} from 'typeorm';
+import { Order } from './order.entity';
+import { User } from 'src/user/entities';
+import { Coupon } from './coupon.entity';
+
+@Entity()
+export class IssuedCoupon extends BaseEntity {
+  @ManyToOne(() => User)
+  @JoinColumn()
+  user: Relation<User>;
+
+  @Column()
+  couponID: string; // 쿠폰 고유 식별번호
+
+  @ManyToOne(() => Coupon)
+  @JoinColumn()
+  coupon: Relation<Coupon>;
+
+  @ManyToOne(() => Order, (order) => order.usedIssuedCoupon, { nullable: true })
+  usedOrder: Relation<Order>;
+
+  @Column({ type: 'boolean', default: false })
+  isValid: boolean;
+
+  @Column({ type: 'timestamp', nullable: false })
+  validFrom: Date;
+
+  @Column({ type: 'timestamp', nullable: false })
+  validUntil: Date;
+
+  @Column({ type: 'boolean', default: false })
+  isUsed: boolean;
+
+  @Column({ type: 'timestamp', nullable: true })
+  usedAt: Date;
+
+  use() {
+    this.isUsed = true;
+    this.isValid = false;
+    this.usedAt = new Date();
+  }
+
+  createCouponID(): void {
+    // 16자리수의 랜덤 string을 만들어냅니다. (Ex - 20240326YNYCBWC6)
+    const dateStr = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+    const randomStr = Array(8)
+      .fill(null)
+      .map(() =>
+        Math.floor(Math.random() * 36)
+          .toString(36)
+          .toUpperCase(),
+      )
+      .join('');
+    this.couponID = dateStr + randomStr;
+  }
+}
