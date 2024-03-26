0caa76dbe99057a190a6ca709d1cf7746d2393fe
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 01:12:18 +0900

    feat
    의도
    coupon에 대한 메서드를 정의해놓기 위함
    
    기능
    coupon을 생성, maxIssue(최대 발급 제한)을 인자로 넣을 시 제한을 걸 수 있음.

---
+++ src/payment/repositories/coupon.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { Coupon, CouponType } from '../entities/coupon.entity';
+
+@Injectable()
+export class CouponRepository extends Repository<Coupon> {
+  constructor(
+    @InjectRepository(Coupon)
+    private readonly repo: Repository<Coupon>,
+    @InjectEntityManager()
+    private readonly entityManager: EntityManager,
+  ) {
+    super(repo.target, repo.manager, repo.queryRunner);
+  }
+
+  async createCoupon(
+    couponType: CouponType,
+    value: number,
+    name: string,
+    description: string,
+    maxIssue?: number,
+  ): Promise<Coupon> {
+    const newCoupon = new Coupon();
+    newCoupon.couponType = couponType;
+    newCoupon.couponName = name;
+    newCoupon.description = description;
+    newCoupon.value = value;
+    if (maxIssue != null) {
+      newCoupon.maxIssue = maxIssue;
+    }
+    this.repo.save(newCoupon);
+    return newCoupon;
+  }
+}
