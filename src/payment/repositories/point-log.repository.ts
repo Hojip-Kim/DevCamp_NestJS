ee8b478a29b5a69edbbc0b45d2dde93169c81e6a
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 23:50:08 +0900

    feat
    의도
    Point Log Entity를 handling 및, 관련된 메서드들을 보관 하기 위함
    
    기능
    use method : point를 사용한 경우로, 사용한 금액 및 사용출처, 사용된 포인트의 경로를 설정합니다.
    add method : point가 추가된 경우로, 추가된 금액 및 금액출처, 추가된 포인트의 경로를 설정합니다.

--- src/payment/repositories/point-log.repository.ts
+++ src/payment/repositories/point-log.repository.ts
@@ -3,6 +3,7 @@ import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 import { User } from 'src/user/entities';
 import { PointLog } from '../entities/point-log.entity';
+import { Point } from '../entities';
 
 @Injectable()
 export class PointLogRepository extends Repository<PointLog> {
@@ -15,4 +16,17 @@ export class PointLogRepository extends Repository<PointLog> {
     super(repo.target, repo.manager, repo.queryRunner);
   }
 
+  use(point: Point, amountToUse: number, reason: string): Promise<PointLog> {
+    const pointLog = new PointLog();
+    pointLog.point = point;
+    pointLog.use(amountToUse, reason);
+    return this.save(pointLog);
+  }
+
+  add(point: Point, amountToAdd: number, reason: string): Promise<PointLog> {
+    const pointLog = new PointLog();
+    pointLog.point = point;
+    pointLog.add(amountToAdd, reason);
+    return this.save(pointLog);
+  }
 }
