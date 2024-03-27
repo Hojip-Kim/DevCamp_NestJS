3c76962ec04231e32179aa34f16b8d600ecf054b
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 23:27:51 +0900

    feat: Payment Module 설정

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,4 +1,72 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  Coupon,
+  IssuedCoupon,
+  Order,
+  OrderItem,
+  Point,
+  PointLog,
+  Product,
+  ShippingInfo,
+} from './entities';
+import { UserModule } from 'src/user/user.module';
+import { PaymentController } from './controller/payment.controller';
+import { PaymentService } from './service/payment.service';
+import {
+  CouponRepository,
+  IssuedCouponRepository,
+  OrderItemRepository,
+  OrderRepository,
+  PointLogRepository,
+  PointRepository,
+  ProductRepository,
+  ShippingInfoRepository,
+} from './repositories';
+import { ProductService } from './service/product.service';
+import { CouponService } from './service/coupon.service';
 
-@Module({})
+@Module({
+  imports: [
+    forwardRef(() => UserModule),
+    TypeOrmModule.forFeature([
+      Coupon,
+      IssuedCoupon,
+      OrderItem,
+      Order,
+      PointLog,
+      Point,
+      Product,
+      ShippingInfo,
+    ]),
+  ],
+  controllers: [PaymentController],
+  providers: [
+    PaymentService,
+    CouponRepository,
+    IssuedCouponRepository,
+    OrderItemRepository,
+    OrderRepository,
+    PointLogRepository,
+    PointRepository,
+    ProductRepository,
+    ShippingInfoRepository,
+    ProductService,
+    CouponService
+  ],
+  exports: [
+    PaymentService,
+    CouponRepository,
+    IssuedCouponRepository,
+    OrderItemRepository,
+    OrderRepository,
+    PointLogRepository,
+    PointRepository,
+    ProductRepository,
+    ShippingInfoRepository,
+    ProductService,
+    CouponService
+  ],
+})
 export class PaymentModule {}
+
