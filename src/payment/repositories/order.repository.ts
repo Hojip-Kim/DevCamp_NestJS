2b3f9b53dc2ec1beef28c4992515a9ffee4a9277
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 23:46:49 +0900

    feat
    의도
    Order Repository의 Order의 상태에 대한 메서드를 제공하기위함.
    
    기능
    사용자가 결제 금액을 지불하면 Order의 status를 'paid'로 설정하고, coupon과 point의 사용상태를 업데이트합니다.

--- src/payment/repositories/order.repository.ts
+++ src/payment/repositories/order.repository.ts
@@ -1,30 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 import { Order } from '../entities/order.entity';
 import { OrderItem } from '../entities/order-item.entity';
 import { ShippingInfo } from '../entities/shipping-info.entity';
 import { UserRepository } from 'src/user/repositories/user.repository';
+import { User } from 'src/user/entities';
+import { IssuedCouponRepository } from './issued-coupon.repository';
+import { PointRepository } from './point.repository';
+import { BusinessException } from 'src/exception/BusinessException';
 
 @Injectable()
-export class OredrRepository extends Repository<Order> {
+export class OrderRepository extends Repository<Order> {
   constructor(
     @InjectRepository(Order)
     private readonly repo: Repository<Order>,
     @InjectEntityManager()
     private readonly entityManager: EntityManager,
-    private readonly userRepository: UserRepository,
+    private readonly issuedCouponRepository: IssuedCouponRepository,
+    private readonly pointRepository: PointRepository,
   ) {
     super(repo.target, repo.manager, repo.queryRunner);
   }
 
   async createOrder(
-    userId: string,
+    user: User,
     orderItems: OrderItem[],
     amount: number,
     shippingInfo?: ShippingInfo,
   ): Promise<Order> {
-    const user = await this.userRepository.findOne({ where: { id: userId } });
     const order = new Order();
     order.user = user;
     order.amount = amount;
@@ -32,5 +36,35 @@ export class OredrRepository extends Repository<Order> {
     order.items = orderItems;
     order.shippingInfo = shippingInfo;
     return this.save(order);
+  }
+
+  async completeOrder(userId: string, orderId: string): Promise<Order> {
+    const order = await this.findOne({
+      where: {
+        id: orderId,
+        user: {
+          id: userId,
+        },
+      },
+    });
+    if (!order) {
+      throw new BusinessException(
+        'payment',
+        'Not-Exist-Order',
+        'Not-Exist-Order',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    order.status = 'paid';
+
+    await Promise.all([
+      this.issuedCouponRepository.use(order.usedIssuedCoupon),
+      this.pointRepository.use(
+        order.user,
+        order.pointAmountUsed,
+        'Order Successful',
+      ),
+    ]);
+    return this.repo.save(order);
   }
 }
