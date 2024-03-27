cae5db3d6987a26c1f190812e3aa13f465d0c7a5
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 23:37:36 +0900

    refactor : status Type 수정

--- src/payment/entities/shipping-info.entity.ts
+++ src/payment/entities/shipping-info.entity.ts
@@ -1,6 +1,6 @@
-import { BaseEntity, Column, Entity, OneToOne, Relation } from 'typeorm';
+import { Column, Entity, OneToOne, Relation } from 'typeorm';
 import { Order } from './order.entity';
-import { text } from 'stream/consumers';
+import { BaseEntity } from 'src/common';
 
 export type shippingType =
   | 'ordered' // 주문 완료
@@ -15,15 +15,14 @@ export class ShippingInfo extends BaseEntity {
   order: Relation<Order>;
 
   @Column()
-  status: ShippingInfo; // 배송 상태
+  status: shippingType; // 배송 상태
 
-  @Column({ type : 'text' })
+  @Column({ type: 'text' })
   address: string; // 주문자 주소
 
-  @Column({nullable : true})
-  trackingNumber : string; // 운송장번호
+  @Column({ nullable: true })
+  trackingNumber: string; // 운송장번호
 
-  @Column({ nullable : true})
-  shippingCompany : string; // 배송 회사
-
+  @Column({ nullable: true })
+  shippingCompany: string; // 배송 회사
 }
