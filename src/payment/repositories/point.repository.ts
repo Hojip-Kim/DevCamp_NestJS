742d568c3f01d760b1fe8bc2252ae192ee8ca6cf
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-28 00:07:25 +0900

    feat
    의도
    Point Enity를 handling 및 관련 메소드들을 보관하기 위함
    
    기능
    use method : 사용한 point만큼 Point Entity에서 차감 및 entity update
    add method : 추가된 point만큼 point entity에서 추가 및 entity update

--- src/payment/repositories/point.repository.ts
+++ src/payment/repositories/point.repository.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 import { Point } from '../entities/point.entity';
+import { PointLogRepository } from './point-log.repository';
+import { User } from 'src/user/entities';
 
 @Injectable()
 export class PointRepository extends Repository<Point> {
@@ -10,7 +12,22 @@ export class PointRepository extends Repository<Point> {
     private readonly repo: Repository<Point>,
     @InjectEntityManager()
     private readonly entityManager: EntityManager,
+    private readonly pointLogRepository: PointLogRepository,
   ) {
     super(repo.target, repo.manager, repo.queryRunner);
+  }
+
+  async use(user: User, amountToUse: number, reason?: string): Promise<Point> {
+    const point = user.point;
+    point.use(amountToUse);
+    await this.pointLogRepository.use(point, amountToUse, reason);
+    return this.save(point);
+  }
+
+  async add(user: User, amountToAdd: number, reason?: string): Promise<Point> {
+    const point = user.point;
+    point.add(amountToAdd);
+    await this.pointLogRepository.add(point, amountToAdd, reason);
+    return this.save(point);
   }
 }
