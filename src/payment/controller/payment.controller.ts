ed067a9947387034d8b6b81c1dd1a5f68d392ef9
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-27 23:30:39 +0900

    feat
    의도
    payment 관련 라우팅 설정을 위함
    
    기능
    주문을 위한 '/payment/order'와, 주문완료를 위한 '/payment/paid'를 통해 order의 전반적 사이클 처리 가능

--- src/payment/controller/payment.controller.ts
+++ src/payment/controller/payment.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateOrderResponseDto } from '../dto/createOrder-Response.dto';
+import { PaymentService } from '../service/payment.service';
+import { CreateOrderRequestDto } from '../dto/createOrder-Request.dto';
+import { Order } from '../entities';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) {}
+
+  // order
+  @Post('/order')
+  @UseGuards(AuthGuard('access-token')) // sub : userId
+  async createOrder(
+    @Req() req,
+    @Body() body: CreateOrderRequestDto,
+  ): Promise<CreateOrderResponseDto> {
+    const { userId } = req.user;
+
+    const order = await this.paymentService.initOrder(userId, body);
+
+    return {
+      orderItems: order.items,
+      couponId: order.usedIssuedCoupon.coupon.id,
+      issuedCouponId: order.usedIssuedCoupon.id,
+      pointAmountToUse: order.pointAmountUsed,
+      shippingAddress: order.shippingInfo.address,
+      totalAmount: order.amount,
+    };
+  }
+
+  @Post('/paid')
+  @UseGuards(AuthGuard('access-token'))
+  async completeOrder(@Req() req, @Body() body): Promise<Order> {
+    const { userId } = req.user;
+    const { orderId } = body;
+
+    return await this.paymentService.completeOrder(userId, orderId);
+  }
+}
