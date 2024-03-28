0ea280d5ef2ac453f49b7c762f60b9251ed3ba0e
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-29 01:37:48 +0900

    feat: nestJS static file use

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BusinessExceptionFilter } from './exception/BusinessExceptionFilter';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.useGlobalFilters(new BusinessExceptionFilter());
+
+  app.use(express.static(join(__dirname, '..', 'public')));
 
   await app.listen(3000);
 }
