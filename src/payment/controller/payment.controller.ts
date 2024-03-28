5198740904ce58f6471da327b09719c003715a9c
Hojip-Kim <rlaghwlq12@naver.com>
2024-03-29 01:45:34 +0900

    feat
    의도
    Toss Payment모듈을 연동하기위한 라우터 설정.
    
    기능
    Toss Payment 모듈을 통해 client단에서 결제하기 버튼 click시 success / fail 라우터로 요청, 각각의 작업을 실행합니다.

--- src/payment/controller/payment.controller.ts
+++ src/payment/controller/payment.controller.ts
@@ -1,15 +1,28 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateOrderResponseDto } from '../dto/createOrder-Response.dto';
 import { PaymentService } from '../service/payment.service';
 import { CreateOrderRequestDto } from '../dto/createOrder-Request.dto';
 import { Order } from '../entities';
+import { TossDto } from '../dto/tossPayment.dto';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
-  // order
+  /* 
+  order
+  결제창으로 넘어갈 때 이 라우터가 실행됩니다.
+
+  */
   @Post('/order')
   @UseGuards(AuthGuard('access-token')) // sub : userId
   async createOrder(
@@ -30,12 +43,19 @@ export class PaymentController {
     };
   }
 
-  @Post('/paid')
-  @UseGuards(AuthGuard('access-token'))
-  async completeOrder(@Req() req, @Body() body): Promise<Order> {
-    const { userId } = req.user;
-    const { orderId } = body;
-
-    return await this.paymentService.completeOrder(userId, orderId);
+  /*
+  payment가 정상적으로 success처리 되었을 경우 Order의 status가 'paid'로 바뀌고,
+   포인트 및 쿠폰 사용처리 완료됨.
+   */
+  @Get('/success')
+  async completeOrder(@Query() query: TossDto): Promise<any> {
+    await this.paymentService.tossPayment(query);
+    return { response: '완료' };
   }
+
+  @Post('/fail')
+  failOrder() {
+    return { response: '실패' };
+  }
+
 }
